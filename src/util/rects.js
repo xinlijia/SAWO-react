81e4549a45f80920b6ed10106d78333cf885b795
jiaxinli <xinli.jia@sjtu.edu.cn>
2018-09-21 22:21:58 -0700

    add rect consts

--- src/util/rects.js
+++ src/util/rects.js
@@ -5,15 +5,24 @@ export const move_list_rect = {
     height: 30,
 };
 
+
+export const maze_rect = {
+    top: 200,
+    left: -3,
+    width: 350,
+    height: 350,
+}
+
 export const timeline_rect = {
-    top: 100,
+    top: 120,
     left: 50,
     width: 250,
     height: 10,
 };
 
+
 export const tool_list_rect = {
-    top: 40,
+    top: 60,
     left: 25,
     width: 300,
     height: 30,
